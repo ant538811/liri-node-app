91761a358843481760bab996cb5ccb6c5312e119
Anthony <an538811@gmail.com>
2017-08-21 10:47:09 -0500

    abc

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'kHykF6sKkxgi68p5hI2UHN6jB',
+  consumer_secret: 'iTvGJ2yqV1K54N9VaQUlUbHiB3TlqbYK1FYfGxFl5WdrQzk7d0',
+  access_token_key: '899295796323110912-42dW6SYWJFyy1V3WInmtHd1PPS41lMt',
+  access_token_secret: 'nPpoBC2rzs6MaUbilefafMF0CpoyqTbxVbd5VIjXhYtTH',
+}
